c1dbcec30ff5c2bcdbbe0b7d287c05efc87e71a8
QuirinEcker <quirin.ecker@bajupa.com>
2021-09-17 13:20:17 +0200

    implemented channel_postfix

--- src/Main/control/VotingDisplayController.ts
+++ src/Main/control/VotingDisplayController.ts
@@ -4,13 +4,16 @@ import InternalError from "../error/InternalError";
 import Logger from "../logger/Logger";
 import GuildConfiguration from "../config/GuildConfiguration";
 import {VoteDisplayType} from "../util/VoteDisplayType";
+import DiscordController from "./DiscordController";
+import {GuildChannel} from "discord.js";
 
 @injectable()
 export default class VotingDisplayController {
 
     constructor(
         @inject(ConfigController) private configController: ConfigController,
-        @inject(Logger) private logger: Logger
+        @inject(Logger) private logger: Logger,
+        @inject(DiscordController) private discordController: DiscordController
     ) {
     }
 
@@ -21,15 +24,15 @@ export default class VotingDisplayController {
                 for (const displayType of guildConfig.votingDisplayTypes) {
                     switch (displayType) {
                         case VoteDisplayType.CHANNEL_NAME:
-                            this.displayChannelName(votingResult)
+                            this.displayChannelName(votingResult, guildConfig)
                                 .catch(reject)
                             break
                         case VoteDisplayType.CHANNEL_NAME_POSTFIX:
-                            this.displayChannelNamePostFix(votingResult)
+                            this.displayChannelNamePostFix(votingResult, guildConfig)
                                 .catch(reject)
                             break
                         case VoteDisplayType.CHANNEL_MESSAGE:
-                            this.displayChannelMessage(votingResult)
+                            this.displayChannelMessage(votingResult, guildConfig)
                                 .catch(reject)
                             break
                     }
@@ -41,21 +44,62 @@ export default class VotingDisplayController {
         })
     }
 
-    private displayChannelName(votingResult: Map<string, number>): Promise<void> {
+    private displayChannelName(votingResult: Map<string, number>, guildConfig: GuildConfiguration): Promise<void> {
         return new Promise((resolve, reject) => {
             this.logger.warn("not implemented")
         })
     }
 
-    private displayChannelNamePostFix(votingResult: Map<string, number>): Promise<void> {
+    private displayChannelNamePostFix(votingResult: Map<string, number>, guildConfig: GuildConfiguration): Promise<void> {
         return new Promise((resolve, reject) => {
+            this.logger.info('displaying ChannelNamePostFix')
 
+            if(guildConfig.votingChannelId && guildConfig.id) {
+                this.discordController.getChannelOf(guildConfig.id, guildConfig.votingChannelId)
+                    .then(channel => channel as GuildChannel)
+                    .then(guildChannel => this.displayChannelNamePostFixIn(guildChannel, votingResult))
+                    .catch(reject)
+            } else {
+                reject(new InternalError("Configuration invalid"))
+            }
         })
     }
 
-    private displayChannelMessage(votingResult: Map<string, number>): Promise<void> {
+    private displayChannelMessage(votingResult: Map<string, number>, guildConfig: GuildConfiguration): Promise<void> {
         return new Promise((resolve, reject) => {
             this.logger.warn("not implemented")
+        })
+    }
+
+    private static getBaseName(name: string) {
+        return name.split('-§-')[0];
+    }
+
+    private displayChannelNamePostFixIn(channel: GuildChannel, votingResult: Map<string, number>): Promise<void> {
+        return new Promise((resolve, reject) => {
+            const basename = VotingDisplayController.getBaseName(channel.name)
+            this.logger.debug(basename)
+            let first = true
+
+            if (votingResult.size === 1) {
+                votingResult.forEach((counts, name) => {
+                    if (first) {
+                        channel.setName(`${basename}-§-${name}`)
+                            .then(() => resolve())
+                            .catch(reject)
+
+                        first = false
+                    }
+
+                    this.logger.debug(`${basename}-§-${name}`);
+                })
+            } else {
+                channel.setName( `${basename}-§-${votingResult.size}-winner`)
+                    .then(() => resolve())
+                    .catch(reject)
+
+                this.logger.debug(`${basename}-§-${votingResult.size} Winner`);
+            }
         })
     }
 }
