cb70f98765138148734f497bfaeed00aaa39712a
QuirinEcker <quirin.ecker@bajupa.com>
2021-09-18 20:41:02 +0200

    implemented announcement in every announcement

--- src/Main/util/announcements/FinalMovieNightAnnouncement.ts
+++ src/Main/util/announcements/FinalMovieNightAnnouncement.ts
@@ -1,3 +1,13 @@
-export default class FinalMovieNightAnnouncement {
+import Announcement from "./Announcement";
+
+export default class FinalMovieNightAnnouncement implements Announcement{
+
+    get description(): string {
+        return "";
+    }
+
+    get title(): string {
+        return ""
+    }
 
 }
--- src/Main/util/announcements/MovieNightReminderAnnouncements.ts
+++ src/Main/util/announcements/MovieNightReminderAnnouncements.ts
@@ -1,3 +1,13 @@
-export default class MovieNightReminderAnnouncements {
+import Announcement from "./Announcement";
+
+export default class MovieNightReminderAnnouncements implements Announcement{
+
+    get description(): string {
+        return "";
+    }
+
+    get title(): string {
+        return ""
+    }
 
 }
