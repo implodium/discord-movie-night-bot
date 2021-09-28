d43d7caf93f4ed52943c6e66b0c1bf91a43c84b6
QuirinEcker <quirin.ecker@bajupa.com>
2021-09-28 08:37:02 +0200

    added syntax for permissions

--- src/Main/commands/Command.ts
+++ src/Main/commands/Command.ts
@@ -1,11 +1,14 @@
 import Option from "./Option";
 import {CommandInteraction} from "discord.js";
+import {PermissionMode} from "../util/PermissionMode";
 
 export default abstract class Command {
 
     name: string
     description: string
     intOptions: Option<number>[]
+    mode: PermissionMode = PermissionMode.BLACKLIST
+    listedRoles: string[] = []
 
     protected constructor(name: string, description: string) {
         this.name = name
@@ -15,7 +18,11 @@ export default abstract class Command {
 
     public abstract exec(interaction: CommandInteraction): Promise<void>
 
-    addIntOption(option: Option<number>) {
-        this.intOptions.push(option)
+    addIntOption(... option: Option<number>[]) {
+        this.intOptions.push(... option)
+    }
+
+    addRoles(... roleIds: string[]) {
+        this.listedRoles.push(... roleIds)
     }
 }
