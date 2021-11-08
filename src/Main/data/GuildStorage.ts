dd4c5a45e63463f8b697cb73e39e568c2a077b71
QuirinEcker <quirin.ecker@bajupa.com>
2021-11-08 13:27:33 +0100

    added property for saving the scheduled movie nights

--- src/Main/data/GuildStorage.ts
+++ src/Main/data/GuildStorage.ts
@@ -1,3 +1,6 @@
+import ScheduledMovieNight from "../util/ScheduledMovieNight";
+
 export default interface GuildStorage {
     winnerMessageId?: string
+    scheduledMovieNights?: Record<string, ScheduledMovieNight>
 }
