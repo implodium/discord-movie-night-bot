2c42da38226ba4ea5af941c3fe12fd0a3cc82642
Quirin Ecker <quirin.ecker@bajupa.com>
2021-11-10 14:17:44 +0100

    events are working now

--- src/Main/control/MovieNightEventController.ts
+++ src/Main/control/MovieNightEventController.ts
@@ -16,31 +16,59 @@ export default class MovieNightEventController {
     }
 
     getMultiplier(message: Message, guildConfig: GuildConfiguration): number {
-        if (guildConfig.id) {
-            const events: EventConfig[] = this.configController
-                .getConfig(`guilds.${guildConfig.id}.events`)
+        let multiplier = 0;
+        const events = this.getEvents(guildConfig)
 
-            const event = events[0]
-
-            if (event.emoji) {
-                if (this.doesMessageHaveEmoji(message, event.emoji)) {
-                    if (event.eventMultiplier) {
-                        return event.eventMultiplier
+        if (events.length !== 0) {
+            for (const event of events) {
+                if (this.isEvent(event)) {
+                    this.logger.debug('is event ' + message.content)
+                    if (event.emoji) {
+                        if (this.doesMessageHaveEmoji(message, event.emoji)) {
+                            if (event.eventMultiplier) {
+                                multiplier += event.eventMultiplier
+                            } else {
+                                multiplier += 2
+                            }
+                        } else {
+                            if (event.normalMultiplier) {
+                                multiplier += event.normalMultiplier
+                            } else {
+                                multiplier += 1
+                            }
+                        }
                     } else {
-                        return 2
-                    }
-                } else {
-                    if (event.normalMultiplier) {
-                        return event.normalMultiplier
-                    } else {
-                        return 1
+                        throw new InternalError('emoji is missing in event configuration')
                     }
                 }
-            } else {
-                throw new InternalError('emoji is missing in event configuration')
             }
+        } else {
+            multiplier = 1
         }
-        return 1
+
+
+
+        this.logger.debug(multiplier)
+
+        return multiplier
+    }
+
+    private isEvent(event: EventConfig): boolean {
+        if (event.start && event.end) {
+            const [eventStartDay, eventStartMonth] = event.start.split('.')
+                .map(text => parseInt(text, 0))
+
+            const [eventEndDay, eventEndMonth] = event.end.split('.')
+                .map(text => parseInt(text, 0))
+
+            const currentDate = new Date()
+            const startDate = new Date(currentDate.getFullYear(), eventStartMonth - 1, eventStartDay, 0)
+            const endDate = new Date(currentDate.getFullYear(), eventEndMonth - 1, eventEndDay, 24)
+
+            return currentDate >= startDate && currentDate <= endDate
+        } else {
+            throw new InternalError('event boundaries not set')
+        }
     }
 
     private doesMessageHaveEmoji(message: Message, emoji: string) {
@@ -48,5 +76,17 @@ export default class MovieNightEventController {
             .valueOf()
             .filter(reaction => reaction.emoji.name === emoji)
             .size !== 0
+    }
+
+    private getEvents(guildConfig: GuildConfiguration): EventConfig[] {
+        if (guildConfig.id) {
+            const events: EventConfig[] = this.configController
+                .getConfig(`guilds.${guildConfig.id}.events`)
+
+            return events
+                .filter(event => this.isEvent(event))
+        } else {
+            throw new InternalError('guild id was not set in configuration')
+        }
     }
 }
